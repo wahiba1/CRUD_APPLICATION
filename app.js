205cc10181d0a43d7da99854ed5f3386fbc7a2f8
Wahiba Akram <wahibaakram8@gmail.com>
2022-05-24 01:07:36 +0500

    user

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/add-user', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CRUD APPLICATION' });
 });
 
+router.get('/add-user', function(req, res, next) {
+  res.render('add_user', { title: 'CRUD APPLICATION' });
+});
+
 
 
 module.exports = router;
